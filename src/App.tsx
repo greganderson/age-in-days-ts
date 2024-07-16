e76069ad20170556d647aaba91272c5f22605189
Greg Anderson <greg.anderson.cs@gmail.com>
2024-07-16 11:24:44 -0600

    Create Calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import Calculator from "./Calculator";
 
 function App() {
   return (
     <>
+      <header>
+        <h1>Age in Days Calculator</h1>
+        <p>Given how many years old someone is, this app will tell you how many days old they are.</p>
+      </header>
+      <main>
+        <Calculator />
+      </main>
     </>
   );
 }
